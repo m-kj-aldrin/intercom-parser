adac8a7cf9f05032b11a1c3530f90fb57345fb81
MKJ <mkj.aldrin@icloud.com>
2023-10-28 22:52:48 +0200

    reexport

---
+++ src/parser/index.js
@@ -0,0 +1,8 @@
+import config from "./config.js";
+import chain from "./chain.js";
+import module from "./module.js";
+import parameter from "./parameter.js";
+import out from "./out.js";
+import periphial from "./periphial.js";
+
+export default { config, chain, module, parameter, out, periphial };
